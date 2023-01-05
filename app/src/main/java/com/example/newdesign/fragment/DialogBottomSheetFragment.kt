ebb7451f2aaa86b80bd232c0519f8b793cb560b9
DESK-ANDROID\osama <usamasaid142@gmail.com>
2023-01-05 17:20:36 +0200

    add select photo and add token to hilt

--- app/src/main/java/com/example/newdesign/fragment/DialogBottomSheetFragment.kt
+++ app/src/main/java/com/example/newdesign/fragment/DialogBottomSheetFragment.kt
@@ -30,6 +30,7 @@ class DialogBottomSheetFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
     }
 
 }
